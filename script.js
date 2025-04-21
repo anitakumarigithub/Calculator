9acac543463fa37b50ab6d0094e77ba817b2d64f
Ecommerce <anitakumari17934@gmail.com>
2025-04-21 11:59:56 +0530

    update project

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display");
+
+function appendNumber(number) {
+  display.value += number;
+}
+
+function appendOperator(operator) {
+  if (display.value !== "" && !isNaN(display.value.slice(-1))) {
+    display.value += operator;
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+   display.value = eval(display.value);  } catch {
+    display.value = "Error";
+  }
+}
